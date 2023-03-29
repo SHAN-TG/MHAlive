264fae52c5dc799ea6edab3d80a065e1da7721d8
SHAN-TG <85439835+SHAN-TG@users.noreply.github.com>
2023-03-29 06:08:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -359,7 +359,7 @@ async def pm_auto_filter(client, msg):
     else:
         return         
     btn = [[
-        InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}",
+        InlineKeyboardButton(text=f"{get_size(file.file_size)} {file.file_name}",
             callback_data=f'pmfile#{file.file_id}')
         ] for file in files ]               
                                    
